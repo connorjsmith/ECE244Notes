9cc23140ec81e7394dd3e50840eebd446454bc49
Connor Smith <connorjsmith101@gmail.com>
2014-09-29 14:59:15 -0400

    Added Sept 29 Notes

--- Sept 29 - Objects and Classes.cpp
+++ Sept 29 - Objects and Classes.cpp
@@ -43,3 +43,20 @@ void DayOfYear::print () {...}
 //These functions are allowed to access object variables, because they can NEVER be 
 //called without first instantiating the object -> its variables are now available
 
+//to prevent malicious member access, you need to ENCAPSULATE members to ensure they cannot be accessed outside of how you intend them to be accessed.
+
+//Better class declaration:
+class DayOfYear {
+	private: //cannot be accessed except by members of this class (ie setDay and setMonth);
+		int day;
+		int month;
+	public: // Accessable by member functions AND outside functions (ie can call FirstOfJuly.setDay(10); in main)
+		void setDay(int d);
+		void setMonth(int m);
+		void print ();
+};
+
+DayOfYear birthday;
+birthday.day = 10; //ERROR!
+int hello = birthday.day; //ERROR! Cannot read OR write
+birthday.setDay(10); // OK!
